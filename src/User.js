41f74c29ae6fa4a4d8fd6f57c5100aaedfc53813
DESKTOP-KF57IBR\PRIYANKA <priyankasatwan@gmail.com>
2022-01-04 23:44:33 +0530

    first commit

---
+++ src/User.js
@@ -0,0 +1,7 @@
+function User(){
+    return(
+        
+        <h1>user components</h1>
+    );
+}
+export default User;
---
+++ src/Userclass.js
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react'
+
+   class Userclass extends Component
+ {
+     render()
+     {
+         return(
+             <div>
+             <h1>hello from userclass</h1>
+             </div>
+         );
+     }
+ }
+
+ export default Userclass;
